7fb4b65711f47ae35add6d539b54787274765f40
Wealong <wealong0@gmail.com>
2014-11-16 18:15:21 +0800

    7

--- src/com/superphonebook/android/activity/MainActivity.java
+++ src/com/superphonebook/android/activity/MainActivity.java
@@ -20,45 +20,49 @@ import com.superphonebook.SuperPhoneBookApplication;
 import com.superphonebook.service.IPersonService;
 
 public class MainActivity extends Activity implements OnClickListener {
-	private ListView nameListView;
-	private Button newPeopleButton;
-	private List<String> nameList = new ArrayList<String>();
-	private IPersonService personService;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		personService = ((SuperPhoneBookApplication)getApplication()).getPersonService();
-		nameListView = (ListView) findViewById(R.id.lv_userlist);
-		newPeopleButton = (Button) findViewById(R.id.newpeople);
-		
-		nameList = personService.getDefaultNameList();
-		newPeopleButton.setOnClickListener(this);
-		ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,nameList);
-		nameListView.setAdapter(myArrayAdapter);
-		nameListView.setOnItemClickListener(new OnItemClickListener() {
+    private ListView nameListView;
+    private Button newPeopleButton;
+    private List<String> nameList = new ArrayList<String>();
+    private IPersonService personService;
 
-			public void onItemClick(AdapterView<?> adapterView, View view, int i,
-					long l) {
-			    Intent intent = new Intent(MainActivity.this,UserActivity.class);
-			    intent.putExtra("personName", nameList.get(i));
-			    startActivity(intent);
-			}
-			
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
+	personService = ((SuperPhoneBookApplication) getApplication())
+		.getPersonService();
+	nameListView = (ListView) findViewById(R.id.lv_userlist);
+	newPeopleButton = (Button) findViewById(R.id.newpeople);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+	nameList = personService.getDefaultNameList();
+	newPeopleButton.setOnClickListener(this);
+	ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this,
+		R.layout.person_name_list_item, nameList);
+	nameListView.setAdapter(myArrayAdapter);
+	nameListView.setOnItemClickListener(new OnItemClickListener() {
 
-	public void onClick(View arg0) {
-		Intent intent02 = new Intent(MainActivity.this,NewPeopleActivity.class);
-		startActivity(intent02);
-		
-	}
+	    public void onItemClick(AdapterView<?> adapterView, View view,
+		    int i, long l) {
+		Intent intent = new Intent(MainActivity.this,
+			UserActivity.class);
+		intent.putExtra("personName", nameList.get(i));
+		startActivity(intent);
+	    }
+
+	});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	// Inflate the menu; this adds items to the action bar if it is present.
+	getMenuInflater().inflate(R.menu.activity_main, menu);
+	return true;
+    }
+
+    public void onClick(View arg0) {
+	Intent intent02 = new Intent(MainActivity.this, NewPeopleActivity.class);
+	startActivity(intent02);
+
+    }
 
 }
